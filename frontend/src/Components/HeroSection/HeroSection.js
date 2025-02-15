4d3963a59f513f11fad3e20198cec777c0201416
Ragu162004 <ragu16102004@gmail.com>
2025-02-15 21:43:20 +0530

    base_code_v_1.0.2

--- frontend/src/Components/HeroSection/HeroSection.js
+++ frontend/src/Components/HeroSection/HeroSection.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { NewsLetter, ExploreButton, WelcomeText } from '../../Containers'
 import styles from './HeroSection.module.css'
+import { eyeCatcher, codeTag, cssTag, jsTag, reactTag, tailTag} from '../../assets'
 
 const HeroSection = () => {
     return (
         <div className={styles.heroSection}>
             <div className={styles.container}>
-                <h1>Welcome to DevCommunity <br /><span className={styles.spanText}>Join Us!</span></h1>
+                <p className={styles.text}>Welcome to DevCommunity <br /><span className={styles.spanText}>Collaborate, Innovate, and Share Your Creations!</span></p>
                 <WelcomeText />
                 <ExploreButton />
+            </div>
+            <div className={styles.imageContainer}>
+                <img src={eyeCatcher} alt="intro" height={300} className={styles.image1}/>
+                <img src={codeTag} alt="intro" height={40} className={styles.image2}/>
+                <img src={cssTag} alt="intro" height={40} className={styles.image3}/>
+                <img src={tailTag} alt="intro" height={40} className={styles.image4}/>
+                <img src={reactTag} alt="intro" height={100} className={styles.image5}/>
             </div>
         </div>
     )
--- frontend/src/assets/index.js
+++ frontend/src/assets/index.js
@@ -1,5 +1,11 @@
 import logoIcon from "./logo.svg";
 import bellIcon from "./bell.svg";
 import userIcon from "./user.png";
+import eyeCatcher from "./lookimage2.webp";
+import codeTag from './codetag.webp'
+import cssTag from './css.webp'
+import jsTag from './jsTag1.webp'
+import reactTag from './reactTag.webp'
+import tailTag from'./tailTag.webp'
 
-export { logoIcon, bellIcon, userIcon };
+export { logoIcon, bellIcon, userIcon, eyeCatcher, codeTag, cssTag, jsTag, reactTag, tailTag};
